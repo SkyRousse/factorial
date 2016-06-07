1586ac4c3a86cbfb8ebd980a26ee4af3e9a7c048
Caleb Stokka and Sky Rousse <sky@>
2016-06-07 14:18:28 -0700

    seperate business logic and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
+//backend logic
+var total = 1;
+
+var factorial = function(userInput) {
+	for (var i = userInput; i > 0; i--) {
+		total *= i;
+	};
+	return total;
+}
+
+
+// user interface logic
 $(function() {
 	$("form#calc").submit(function(event) {
     event.preventDefault();
-
-    var total = 1;
     var number = Math.abs(parseInt($("input#factorialInput").val()));
-    //var number = parseInt($("input#number").val());
-    for (var i = number; i > 0; i--) {
-      total *= i;
-    };
+		factorial(number);
+
     alert(total);
   });
 });
